3d2b43c5c66ea0123f6c905edb3f491ce5297d4a
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-03-16 00:29:14 -0300

    recuperado

---
+++ src/scraper.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import * as cheerio from 'cheerio'
+import { writeFile } from 'fs/promises'
+
+const url = 'https://tsrd.fandom.com/pt-br/wiki/Talentos_de_Combate'
+async function scrape(url){
+    axios.create()
+    axios.get(url).then( Response => {
+    const html = Response.data
+    const $ = cheerio.load(html)
+    const $h = $('h3').text()
+    console.log($h)
+    //writeFile('talentos.json',JSON.stringify($p))
+    }
+    )
+}
+
+scrape(url)
