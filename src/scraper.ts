a837ab41e6e8b28259a05290e9c288c76b07880a
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-03-17 09:09:05 -0300

    - axios

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,18 +1,16 @@
-import axios from "axios";
 import * as cheerio from 'cheerio'
 import { writeFile } from 'fs/promises'
 
 const url = 'https://tsrd.fandom.com/pt-br/wiki/Talentos_de_Combate'
 async function scrape(url){
-    axios.create()
-    axios.get(url).then( Response => {
-    const html = Response.data
+    const res = await fetch(url)
+    const html = await res.text()
     const $ = cheerio.load(html)
     const $h = $('h3').text()
-    //console.log($h)
+//    console.log($h)
     writeFile('talentos.json',JSON.stringify($h))
     }
-    )
-}
+    
+
 
 scrape(url)
