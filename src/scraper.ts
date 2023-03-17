79293521cdd62105c4ca884b25816f7ed2e651a6
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-03-17 13:57:27 -0300

    appendFile

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,16 +1,19 @@
 import * as cheerio from 'cheerio'
-import { writeFile } from 'fs/promises'
+import { writeFile, appendFile } from 'fs/promises'
 
 const url = 'https://tsrd.fandom.com/pt-br/wiki/Talentos_de_Combate'
 async function scrape(url){
     const res = await fetch(url)
     const html = await res.text()
     const $ = cheerio.load(html)
-    const $h = $('h3').text()
-//    console.log($h)
-    writeFile('talentos.json',JSON.stringify($h))
+    const quant = $('div').find('h3')
+    for(let i = 0; i <= quant.length; i++){
+    const $h = $('h3').eq(i).text()
+//    const $p = $('p').text()
+    console.log($h)
+    appendFile('talentos.json',JSON.stringify($h, null, 2))
     }
-    
+}    
 
 
 scrape(url)
