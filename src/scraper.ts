6422149a47b95790c7e51f73997dec808b9aecb1
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-03-18 10:16:51 -0300

    correçao de bugs

--- src/scraper.ts
+++ src/scraper.ts
@@ -11,17 +11,22 @@ async function scrape(url){
     const res = await fetch(url)
     const html = await res.text()
     const $ = cheerio.load(html)
-    const quant = $('div').find('h3')
-    for(let i = 0; i <= quant.length; i++){
-    const $h = $('h3').eq(i).text()
+    const quant = $('div').find('div > h3')
+    writeFile('talentos.json','[')
+    for(let i = 0; i < quant.length; i++){
+    const $h = $('div > h3').eq(i).text()
     // const $p = $('p').text()
 
     const tal = new talentos
     tal.nome = $h
     // tal.P = $p
     // console.log($h)
-    appendFile('talentos.json',JSON.stringify(tal, null, 2))
+    await appendFile('talentos.json',JSON.stringify(tal, null, 1))
+    if(i < quant.length - 1){
+    appendFile('talentos.json',',')
+        }
     }
+    appendFile('talentos.json',']')
 }    
 
 
