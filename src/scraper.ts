ff823812fb30dd7f523a6c5c3305a8538da861c2
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-03-18 09:40:00 -0300

    class

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,6 +1,11 @@
 import * as cheerio from 'cheerio'
 import { writeFile, appendFile } from 'fs/promises'
 
+class talentos {
+    nome: String
+    P: String 
+}
+
 const url = 'https://tsrd.fandom.com/pt-br/wiki/Talentos_de_Combate'
 async function scrape(url){
     const res = await fetch(url)
@@ -9,9 +14,13 @@ async function scrape(url){
     const quant = $('div').find('h3')
     for(let i = 0; i <= quant.length; i++){
     const $h = $('h3').eq(i).text()
-//    const $p = $('p').text()
-    console.log($h)
-//    appendFile('talentos.json',JSON.stringify($h, null, 2))
+    // const $p = $('p').text()
+
+    const tal = new talentos
+    tal.nome = $h
+    // tal.P = $p
+    // console.log($h)
+    appendFile('talentos.json',JSON.stringify(tal, null, 2))
     }
 }    
 
