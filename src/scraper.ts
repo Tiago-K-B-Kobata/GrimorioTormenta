06c886eb74504dea616ecb29aa0e4559cd490388
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-03-20 08:01:09 -0300

    sem requsito

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,9 +1,11 @@
 import * as cheerio from 'cheerio'
 import { writeFile, appendFile } from 'fs/promises'
 
-class talentos {
+class talento {
     nome: String
-    P: String 
+    requisito: String
+    beneficio: String
+    especial: String
 }
 
 const url = 'https://tsrd.fandom.com/pt-br/wiki/Talentos_de_Combate'
@@ -11,19 +13,28 @@ async function scrape(url){
     const res = await fetch(url)
     const html = await res.text()
     const $ = cheerio.load(html)
-    const quant = $('div').find('div > h3')
+    const quantH = $('div').find('div > h3')
+    const quantP = $('div').find('div > p')
+   
     writeFile('talentos.json','[')
-    for(let i = 0; i < quant.length; i++){
-    const $h = $('div > h3').eq(i).text()
-    // const $p = $('p').text()
-
-    const tal = new talentos
-    tal.nome = $h
-    // tal.P = $p
-    // console.log($h)
-    await appendFile('talentos.json',JSON.stringify(tal, null, 1))
-    if(i < quant.length - 1){
-    appendFile('talentos.json',',')
+    
+    for(let i = 0; i < quantH.length; i++){
+        const $h = $('div > h3').eq(i).text()
+        const $r = $('p > b').eq(i).nextUntil('h3').text()
+        
+        
+        const tal = new talento
+        
+        tal.nome = $h  
+        if($r == $('p > b:contains(Pré-requisito:)').text())
+            tal.requisito = $('p:contains(Pré-requisito:)').text()
+        // console.log($h)
+        
+        
+        
+        await appendFile('talentos.json',JSON.stringify(tal, null, 1))
+        if(i < quantH.length - 1){
+            appendFile('talentos.json',',')
         }
     }
     appendFile('talentos.json',']')
