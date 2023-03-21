129a549e15a6535ec213ab73569c63377edc84e6
Tiago-K-B-Kobata <tiago_kobata@hotmail.com>
2023-03-21 17:45:28 -0300

    correções

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,7 +1,6 @@
 import * as cheerio from 'cheerio'
 import { writeFile, appendFile } from 'fs/promises'
 
-
 class talento {
     nome: String
     descrição: String
@@ -13,6 +12,7 @@ class talento {
 let TALENTO:any = []
 
 const url = 'https://tsrd.fandom.com/pt-br/wiki/Talentos_de_Combate'
+
 async function scrape(url){
     const res = await fetch(url)
     const html = await res.text()
@@ -21,25 +21,19 @@ async function scrape(url){
        
     for(let i = 0; i < quantH.length; i++){
         const $h = $('div > h3').eq(i).text()
-        const $r = $('div > h3').eq(i).nextUntil('h3').has('b:contains(Pré-requisito),b:contains(Pré-requisitos),b:contains(Pré-requisíto)').text()
-        const $b = $('div > h3').eq(i).nextUntil('h3').has('b:contains(Benefício:)').text()
-        const $e = $('div > h3').eq(i).nextUntil('h3').has('b:contains(Especial:)').text()
+        const $r = $('div > h3').eq(i).nextUntil('h3').has('b:contains(Pré-requisito),b:contains(Pré-requisíto)').text()
+        const $b = $('div > h3').eq(i).nextUntil('h3').has('b:contains(Benefício)').text()
+        const $e = $('div > h3').eq(i).nextUntil('h3').has('b:contains(Especial)').text()
         const $d = $('div > h3').eq(i).nextUntil('h3').has('p > i').text()
 
         
         const tal = new talento
         
-        tal.nome = $h  
-        
+        tal.nome = $h        
         tal.descrição = $d
-
-        tal.requisito = $r
-        
-        tal.beneficio = $b
-        
-        tal.especial = $e
-        
-        
+        tal.requisito = $r        
+        tal.beneficio = $b        
+        tal.especial = $e       
         
         TALENTO.push(tal)
         
@@ -47,6 +41,5 @@ async function scrape(url){
     
     writeFile('talentos.json',JSON.stringify(TALENTO, null, 2))
 }    
-
 
 scrape(url)
